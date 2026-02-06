61465f9ee9ecf70bfedd1a0779d1efddbc613420
venki0722 <mvenkatachalam633@gmail.com>
2026-02-06 14:21:59 +0530

    Create sample.py

---
+++ sample.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.title("Hello World!")
+st.write("This is my first Streamlit app.")
+
+# You can also use magic commands (placing a variable on its own line)
+"Here is some text using magic commands." 
+
+# Add an interactive slider
+x = st.slider("Select a value")
+st.write(x, "squared is", x * x)
